c159f4f72fc811e64492287a84b1d7247373221b
sryang <sarang628@gmail.com>
2025-12-10 14:12:19 +0900

    add my like

--- library/src/main/java/com/sarang/torang/compose/profile/components/ProfileTabs.kt
+++ library/src/main/java/com/sarang/torang/compose/profile/components/ProfileTabs.kt
@@ -1,30 +1,19 @@
 package com.sarang.torang.compose.profile.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.rounded.AccountCircle
-import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
@@ -39,7 +28,7 @@ internal fun ProfileTabs(
                 pagerState.animateScrollToPage(0)
             }
         }) {
-            Icon(Icons.Rounded.AccountCircle, null)
+            Icon(Icons.AutoMirrored.Default.List, null)
         }
 
         IconButton({
@@ -47,7 +36,7 @@ internal fun ProfileTabs(
                 pagerState.animateScrollToPage(1)
             }
         }) {
-            Icon(Icons.Rounded.Face, null)
+            Icon(Icons.Rounded.Favorite, null)
         }
 
         IconButton({
@@ -55,7 +44,7 @@ internal fun ProfileTabs(
                 pagerState.animateScrollToPage(2)
             }
         }) {
-            Icon(Icons.Rounded.Favorite, null)
+            Icon(Icons.Rounded.Star, null)
         }
     }
 }
